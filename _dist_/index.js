772ef4ee00f8374a7f49e37c5471d5be69966726
Garrett Brown <themagnificentmrb@gmail.com>
2022-02-03 21:37:53 -0800

    Updates

---
+++ _dist_/index.js
@@ -0,0 +1,6 @@
+console.log("-------------------------------------");
+console.log(`${document.title}`);
+console.log("-------------------------------------");
+window.addEventListener("load", async () => {
+  console.log("Entering application");
+});
