9f1fd02132eff568e772d2b6c8f57e928873db62
Garrett Brown <themagnificentmrb@gmail.com>
2022-02-04 21:50:50 -0800

    Updates

--- _dist_/index.js
+++ _dist_/index.js
@@ -2,16 +2,16 @@ import QrScanner from "./qr-scanner.js";
 console.log("-------------------------------------");
 console.log(`${document.title}`);
 console.log("-------------------------------------");
-function onClient() {
+async function onClient() {
   const videoPreview = document.getElementById("videoPreview");
   if (videoPreview !== null) {
-    const kodiLogo = document.getElementById("kodiLogo");
-    if (kodiLogo) {
-      kodiLogo.style.display = "none";
+    const displayArea = document.getElementById("displayArea");
+    if (displayArea) {
+      displayArea.style.display = "none";
     }
     videoPreview.style.display = "block";
-    const qrScanner = new QrScanner(videoPreview, (result) => console.log("decoded qr code:", result), {});
-    qrScanner.start();
+    const qrScanner = new QrScanner(videoPreview, (result) => alert(`decoded qr code: ${result}`), {});
+    await qrScanner.start();
   }
 }
 const contentArea = document.getElementById("contentArea");
